ff2e4dc7b9edb315406c4055bb6334bff609aa1c
PatrickTangwen <102566928+PatrickTangwen@users.noreply.github.com>
2023-12-11 20:09:26 -0800

    login system pushed

---
+++ login_system_frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Register from './Register';
+import NavBar from './Navbar';
+
+function App() {
+
+  return (
+    <main className="App">
+      {/* <NavBar /> */}
+      <Register />
+    </main>
+  );
+}
+
+export default App;
